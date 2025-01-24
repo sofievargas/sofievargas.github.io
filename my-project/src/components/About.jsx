7e784022ee61225ce67c67f8e5b8dfa6a738fd52
Sofie Vargas <sofievargas06@gmail.com>
2025-01-23 17:08:33 -0700

    Initial commit

--- my-project/src/components/About.jsx
+++ my-project/src/components/About.jsx
@@ -3,9 +3,19 @@ import React from "react";
 const About = () => {
     return(
 
-        <div className= "flex w-5/6 bg-customLightGreen text-xl text-customBrown font-amarante p-8">
-           <h3>My name is Sofia Vargas, and I am currently a student of Barrett, the Honors College at Arizona State University.</h3>
-           I am majoring in computer science with a minor in math, and I have experience in various programming languages.
+        <div className= "flex w-5/6 rounded-xl bg-customLightGreen text-xl text-left text-customBrown font-amarante p-8">
+           <h2>My name is Sofia Vargas, and I am a student of Barrett, the Honors College at Arizona State University majoring in Computer Science with a minor in Math.
+            <br></br><br></br>
+           My interests mainly lie in combining technology with health and sustainability. 
+           I pursue these through research experiences in diverse topics pertaining to urban heat, species conservation, 
+           network theory, and biosecurity. I also enjoy working with peers to promote engineering opportunities for students
+           through my involvement in the Grand Challenges Scholars Program and ASU's Women in Computer Science organization.
+           This summer, I am excited to gain industry experience with StateFarm as a Software Engineer Intern. 
+           <br></br><br></br>
+           When I am not at school, I enjoy swimming and food blogging in sunny San Diego! Feel free to browse through my past projects and find
+           my contact information below.
+
+           </h2>
         </div>
 
     );
